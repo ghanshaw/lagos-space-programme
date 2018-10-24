67d4e9834105c65287f91bdc2142b95a5ab5c7e1
Greer Hanshaw <hanshawg@amazon.com>
2018-10-23 22:47:33 -0400

    Updates

---
+++ precache-manifest.bdfc3b5e01112b3c8d36c53d593fc02f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lagos-space-programme/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ab3ac81f0a10c78d891a",
+    "url": "/lagos-space-programme/static/js/runtime~main.ab3ac81f.js"
+  },
+  {
+    "revision": "0f14656716ae22f53517",
+    "url": "/lagos-space-programme/static/js/main.0f146567.chunk.js"
+  },
+  {
+    "revision": "c8815e50dfdc1c19009c",
+    "url": "/lagos-space-programme/static/js/1.c8815e50.chunk.js"
+  },
+  {
+    "revision": "0f14656716ae22f53517",
+    "url": "/lagos-space-programme/static/css/main.ab6a7704.chunk.css"
+  },
+  {
+    "revision": "38e6794d0f00a5d433d104608bc4234d",
+    "url": "/lagos-space-programme/index.html"
+  }
+];
